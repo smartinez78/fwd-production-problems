90c00ce8dc60a55bb0b68596cbfe1fbbc1e9cf86
Sofia Martinez <smartinez@hawk.iit.edu>
2018-11-11 21:17:25 -0600

    Add comment statement and preliminary function structure

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,13 @@
+// function to play doorbell audio
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+(function(){
+  if (typeof(document.querySelector)==="undefined") {
+    return;
+  }
+
+  
+})();
